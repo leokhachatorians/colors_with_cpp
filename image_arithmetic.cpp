bd650a4d12ef8fef8aff7b1cb1989b07d9624cc2
Leo Khachatorians <leokhachatorians@gmail.com>
2017-02-16 18:35:13 -0800

    Min and Max

--- image_arithmetic.cpp
+++ image_arithmetic.cpp
@@ -10,6 +10,8 @@ void add_images(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorR
 void subtract_images(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorRGB>, unsigned long, unsigned long);
 void difference_of_images(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorRGB>, unsigned long, unsigned long);
 void crossfade(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorRGB>, unsigned long, unsigned long);
+void min_of_images(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorRGB>, unsigned long, unsigned long);
+void max_of_images(std::vector<ColorRGB>, std::vector<ColorRGB>, std::vector<ColorRGB>, unsigned long, unsigned long);
 
 int main(int argc, char *argv[]) {
     unsigned long w = 0, h = 0;
@@ -26,7 +28,10 @@ int main(int argc, char *argv[]) {
     //add_images(image3, image2, result, h, w);
     //subtract_images(image2, image3, result, h, w);
     //average_images(image1, image3, result, h, w);
-    crossfade(image1, image2, result, h, w);
+    //crossfade(image1, image2, result, h, w);
+    //min_of_images(image1, image2, result, h, w);
+    max_of_images(image1, image2, result, h, w);
+    
 
     redraw();
     sleep();
@@ -101,5 +106,31 @@ void crossfade(std::vector<ColorRGB> image1, std::vector<ColorRGB> image2,
             }
         }
         redraw();
+    }
+}
+
+void min_of_images(std::vector<ColorRGB> image1, std::vector<ColorRGB> image2,
+                 std::vector<ColorRGB> result, unsigned long h, unsigned long w) {
+    for (unsigned long y = 0; y < h; y++) {
+        for (unsigned long x = 0; x < w; x++) {
+            int sum = y * w + x;
+            result[sum].r = std::min(image1[sum].r, image2[sum].r);
+            result[sum].g = std::min(image1[sum].g, image2[sum].g);
+            result[sum].b = std::min(image1[sum].b, image2[sum].b);
+            pset(x, y, result[sum]);
+        }
+    }
+}
+
+void max_of_images(std::vector<ColorRGB> image1, std::vector<ColorRGB> image2,
+                 std::vector<ColorRGB> result, unsigned long h, unsigned long w) {
+    for (unsigned long y = 0; y < h; y++) {
+        for (unsigned long x = 0; x < w; x++) {
+            int sum = y * w + x;
+            result[sum].r = std::max(image1[sum].r, image2[sum].r);
+            result[sum].g = std::max(image1[sum].g, image2[sum].g);
+            result[sum].b = std::max(image1[sum].b, image2[sum].b);
+            pset(x, y, result[sum]);
+        }
     }
 }
